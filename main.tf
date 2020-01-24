dfbb68c508b9ca7cdbfb9f2451552039be18a429
Mathieu Duczmanski <mduczmanski@scalair.fr>
2020-01-24 15:57:24 +0100

    Updating output and removing some decprecated fields

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_storage_account" "this" {
   name                      = var.sa_name
-  resource_group_name       = var.resource_group_name
+  resource_group_name       = data.azurerm_resource_group.this.name
   location                  = var.location
   account_kind              = var.account_kind
   account_tier              = var.account_tier
@@ -17,15 +21,13 @@ resource "azurerm_storage_account" "this" {
 resource "azurerm_storage_container" "this" {
   count                 = length(var.containers)
   name                  = var.containers[count.index].name
-  resource_group_name   = azurerm_storage_account.this.resource_group_name
   storage_account_name  = azurerm_storage_account.this.name
   container_access_type = var.containers[count.index].access_type
 }
 
-resource "azurerm_storage_share" "main" {
+resource "azurerm_storage_share" "this" {
   count                = length(var.shares)
   name                 = var.shares[count.index].name
-  resource_group_name  = azurerm_storage_account.this.resource_group_name
   storage_account_name = azurerm_storage_account.this.name
   quota                = var.shares[count.index].quota
 }
--- output.tf
+++ output.tf
@@ -1,91 +1,90 @@
 # Storage account outputs
 
 output storage_account_id {
-  value = azurerm_storage_account.storage_account.id
+  value = azurerm_storage_account.this.id
 }
 
 output storage_account_name {
-  value = azurerm_storage_account.storage_account.name
+  value = azurerm_storage_account.this.name
 }
 
 output storage_account_kind {
-  value = azurerm_storage_account.storage_account.account_kind
+  value = azurerm_storage_account.this.account_kind
 }
 
 output storage_account_account_tier {
-  value = azurerm_storage_account.storage_account.account_tier
-}
-
-output storage_account_access_tier {
-  value = azurerm_storage_account.storage_account.access_tier
+  value = azurerm_storage_account.this.account_tier
 }
 
 output storage_account_replication_type {
-  value = azurerm_storage_account.storage_account.account_replication_type
+  value = azurerm_storage_account.this.account_replication_type
 }
 
 output storage_account_encryption_source {
-  value = azurerm_storage_account.storage_account.account_encryption_source
+  value = azurerm_storage_account.this.account_encryption_source
 }
 
 output storage_account_primary_location {
-  value = azurerm_storage_account.storage_account.primary_location
+  value = azurerm_storage_account.this.primary_location
 }
 
 output storage_account_secondary_location {
-  value = azurerm_storage_account.storage_account.secondary_location
+  value = azurerm_storage_account.this.secondary_location
 }
 
 output storage_account_primary_blob_endpoint {
-  value = azurerm_storage_account.storage_account.primary_blob_endpoint
+  value = azurerm_storage_account.this.primary_blob_endpoint
 }
 
 output storage_account_primary_queue_endpoint {
-  value = azurerm_storage_account.storage_account.primary_queue_endpoint
+  value = azurerm_storage_account.this.primary_queue_endpoint
 }
 
 output storage_account_primary_table_endpoint {
-  value = azurerm_storage_account.storage_account.primary_table_endpoint
+  value = azurerm_storage_account.this.primary_table_endpoint
 }
 
 output storage_account_primary_file_endpoint {
-  value = azurerm_storage_account.storage_account.primary_file_endpoint
+  value = azurerm_storage_account.this.primary_file_endpoint
 }
 
 output storage_account_primary_access_key {
-  value = azurerm_storage_account.storage_account.primary_access_key
+  value = azurerm_storage_account.this.primary_access_key
 }
 
 output storage_account_secondary_access_key {
-  value = azurerm_storage_account.storage_account.secondary_access_key
+  value = azurerm_storage_account.this.secondary_access_key
 }
 
 output storage_account_primary_connection_string {
-  value = azurerm_storage_account.storage_account.primary_connection_string
+  value = azurerm_storage_account.this.primary_connection_string
 }
 
-output "storage_account_primary_blob_string" {
-  value = azurerm_storage_account.storage_account.primary_blob_connection_string
+output storage_account_primary_blob_string {
+  value = azurerm_storage_account.this.primary_blob_connection_string
 }
 
 # Storage container outputs
 
-output storage_container_ids {
-  value = azurerm_storage_container.storage_container.*.id
+output "containers" {
+  value = {
+    for i in azurerm_storage_container.this :
+    i.name => {
+      id   = i.id
+      name = i.name
+    }
+  }
+  description = "Map of containers."
 }
 
-output storage_containers_names {
-  value = azurerm_storage_container.storage_container.*.name
-}
+# Storage share outputs
 
-output storage_container_sa_name {
-  value = azurerm_storage_container.storage_container.*.storage_account_name
-}
-
-output storage_container_access_type {
-  value = azurerm_storage_container.storage_container.*.container_access_type
-}
-
-output storage_container_properties {
-  value = azurerm_storage_container.storage_container.*.properties
+output "shares" {
+  value = { for j in azurerm_storage_share.this :
+    j.name => {
+      id   = j.id
+      name = j.name
+    }
+  }
+  description = "Map of shares."
 }
